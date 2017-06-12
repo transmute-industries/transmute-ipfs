dd1df6054a6d0d8e484bd4fda0bb80da7e44d27f
Orie Steele <orie@ohs.io>
2017-06-11 21:37:49 -0500

    add web tests

--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
 const ipfsAPI = require('ipfs-api')
-// const through = require('through2')
-// const concat = require('concat-stream')
 
 // IPFS Gateway
 // https://ipfs.infura.io 
 // IPFS RPC
 // https://ipfs.infura.io:5001 
-
-
-// const readFilesFromHashAsync = (hash) => {
-//     return ipfs.files.get(hash).then((stream) => {
-//         let files = []
-//         return new Promise((resolve, reject) => {
-//             stream.pipe(through.obj((file, enc, next) => {
-//                 file.content.pipe(concat((content) => {
-//                     files.push({
-//                         path: file.path,
-//                         content: content.toString()
-//                     })
-//                     next()
-//                 }))
-//             }, () => {
-//                 resolve(files)
-//             }))
-//         })
-//     })
-// }
-
 
 class TransmuteIpfs {
 
@@ -54,13 +31,48 @@ class TransmuteIpfs {
             })
         })
     }
-    // TODO::
-    // ipfs.util.addFromURL('http://example.com/', (err, result) => {
-    // if (err) {
-    // throw err
-    // }
-    // console.log(result)
-    // })
+
+    addFromURL(url) {
+        return new Promise((resolve, reject) => {
+            this.ipfs.util.addFromURL(url, (err, result) => {
+                if (err) {
+                    reject(err)
+                }
+                resolve(result)
+            })
+        })
+    }
+
+    writeObject(obj) {
+        let buffer = new Buffer(JSON.stringify(obj))
+        return new Promise((resolve, reject) => {
+            this.ipfs.add(buffer, (err, res) => {
+                if (err || !res) {
+                    reject(err)
+                }
+                resolve(res[0].path)
+            })
+        })
+    }
+
+    readObject(path) {
+    
+        return new Promise((resolve, reject) => {
+            this.ipfs.cat(path, (err, stream) => {
+                if (err) {
+                    reject(err)
+                }
+                let data = ''
+                stream.on('data', (d) => {
+                    data = data + d
+                })
+                stream.on('end', () => {
+                    let obj = JSON.parse(data)
+                    resolve(obj)
+                })
+            })
+        })
+    }
 
 }
 
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,14 +1,12 @@
-var assert = require('assert');
-
+var assert = require('assert')
 let TI = require('./index')
 
 describe('TransmuteIpfs', () => {
-
     describe('#init()', () => {
         it('should use infura by default', () => {
             TI.init()
-            assert.equal(TI.config.host, 'ipfs.infura.io');
-        });
+            assert.equal(TI.config.host, 'ipfs.infura.io')
+        })
         it('should support local ipfs node', () => {
             TI.init({
                 host: 'localhost',
@@ -17,11 +15,9 @@ describe('TransmuteIpfs', () => {
                     protocol: 'http'
                 }
             })
-            assert.equal(TI.config.host, 'localhost');
-        });
-    });
-
-    // https://github.com/ipfs/js-ipfs-api#utility-functions
+            assert.equal(TI.config.host, 'localhost')
+        })
+    })
     describe('#addFromFs(folderPath, options)', () => {
         before(() => {
             TI.init({
@@ -35,33 +31,63 @@ describe('TransmuteIpfs', () => {
         it('should add folder to ipfs and return the result', () => {
             return TI.addFromFs('./dist')
                 .then((res) => {
-                    console.log(res)
                     assert.equal(res[0].path, 'dist/data.json')
                     assert.equal(res[1].path, 'dist/index.html')
                     assert.equal(res[2].path, 'dist')
                 })
-        });
-    });
-
-    // describe('#getObjectFromHash(hash)', () => {
-    //     before(() => {
-    //         TI.init({
-    //             host: 'localhost',
-    //             port: '5001',
-    //             options: {
-    //                 protocol: 'http'
-    //             }
-    //         })
-    //     })
-    //     it('should add folder to ipfs and return the result', () => {
-    //         return TI.getObjectFromHash('QmRa8NrWWxRa1TTenL8V3en9hbpQdPpjaURkz68wL7NfKU')
-    //             .then((res) => {
-    //                 console.log(res)
-    //                 assert.equal(res[0].path, 'dist/data.json')
-    //                 assert.equal(res[1].path, 'dist/index.html')
-    //                 assert.equal(res[2].path, 'dist')
-    //             })
-    //     });
-    // });
-
-});
+        })
+    })
+    describe('#addFromURL(url)', () => {
+        before(() => {
+            TI.init({
+                host: 'localhost',
+                port: '5001',
+                options: {
+                    protocol: 'http'
+                }
+            })
+        })
+        it('should add folder to ipfs and return the result', () => {
+            return TI.addFromURL('https://vignette3.wikia.nocookie.net/nyancat/images/7/7c/Nyan_gary.gif')
+                .then((res) => {
+                    assert.equal(res[0].path, 'QmSAKHa2JdKrhmBKrqWrBAtS7ACwofiRAEWzYNbXdssBEe')
+                    assert.equal(res[0].hash, 'QmSAKHa2JdKrhmBKrqWrBAtS7ACwofiRAEWzYNbXdssBEe')
+                })
+        })
+    })
+    describe('#writeObject(obj)', () => {
+        before(() => {
+            TI.init({
+                host: 'localhost',
+                port: '5001',
+                options: {
+                    protocol: 'http'
+                }
+            })
+        })
+        it('should add json object to ipfs and return the path', () => {
+            let obj = { cool: 'story...bro' }
+            return TI.writeObject(obj)
+                .then((path) => {
+                    assert.equal(path, 'Qmf4GZbciiPLMTZYLpM88GB2CpopCJszdwybUnnwswkpKE')
+                })
+        })
+    })
+    describe('#readObject(path)', () => {
+        before(() => {
+            TI.init({
+                host: 'localhost',
+                port: '5001',
+                options: {
+                    protocol: 'http'
+                }
+            })
+        })
+        it('should add json object to ipfs and return the path', () => {
+            return TI.readObject('Qmf4GZbciiPLMTZYLpM88GB2CpopCJszdwybUnnwswkpKE')
+                .then((obj) => {
+                    assert.equal(obj.cool, 'story...bro')
+                })
+        })
+    })
+})
